f39a219f5994f3e4201598c7a33ea493ec2ca59d
EthanHess <ech1988@gmail.com>
2015-07-03 11:05:23 -0600

    initial commit, logs into facebook

---
+++ Colorize/Colorize/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Colorize
+//
+//  Created by Ethan Hess on 7/3/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
